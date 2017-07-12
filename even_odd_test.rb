849743369a46fd66be3282355ecf20186a79b8d8
vcmccormick <vcmccormick@gmail.com>
2017-07-12 14:10:54 -0400

    first basic test

---
+++ even_odd_test.rb
@@ -0,0 +1,10 @@
+require "minitest/autorun"
+require_relative "even_odd.rb"
+
+class TestEvenOdd < Minitest::Test #names the type of test you are running
+
+	def test_assert_that_1_equals_1 
+		assert_equal(1, 1) #asserts that a given is equal to what is tried
+	end
+
+end
